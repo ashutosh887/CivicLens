7eb46a07be6024282f9e5bc42adb574f48cc820f
Ashutosh Jha <ashutoshj887@gmail.com>
2025-12-11 01:14:20 +0530

    feat: init shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import config from "@/config";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold text-center">{config.appName}</h1>
+      <h1 className="text-4xl font-bold text-center text-primary">{config.appName}</h1>
       <h4 className="text-lg text-gray-500 text-center mt-4">{config.appDescription}</h4>
+
+      <div className="mt-4 space-x-2">
+        <Button variant="default" className="cursor-pointer">Get Started</Button>
+        <Button variant="outline" className="cursor-pointer">Learn More</Button>
+      </div>
     </div>
   )
 }
